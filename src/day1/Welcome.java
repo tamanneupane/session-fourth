71be12308f5d1e44c785feb1be233c150754683b
Taman Neupane <aashish.karn@peacenepal.com>
2021-11-20 07:31:13 +0545

    Message Changed for greetings

--- src/day1/Welcome.java
+++ src/day1/Welcome.java
@@ -3,7 +3,7 @@ package day1;
 public class Welcome {
 
     public static void main(String[] args) {
-        String greeting = "Welcome to Core Java session fourth!";
+        String greeting = "Welcome to Core Java session fourth 2021!";
         System.out.println(greeting);
         for (int i = 0; i < greeting.length(); i++) {
             System.out.print("=");
